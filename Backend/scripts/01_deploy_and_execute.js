5f960bac902497df72417cb85793b9e7027f79f1
Ayan Panchariya <ayanpanchariya165@gmail.com>
2023-05-31 23:58:54 +0530

    final changes

--- Backend/scripts/01_deploy_and_execute.js
+++ Backend/scripts/01_deploy_and_execute.js
@@ -5,6 +5,36 @@ const ethers = hre.ethers;
 async function main() {
   // Deploy contracts
   await deployContracts();
+
+  // List a property
+  // await listProperty(
+  //   "0x851356ae760d987E095750cCeb3bC6014560891C",
+  //   "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  //   "https://raw.githubusercontent.com/Sud0-AP/web3athon/main/Backend/contracts/Nft_metadata/metadata.json",
+  //   "ArxRealEstate",
+  //   "A 0014, 68th st. Los Santos, San Andreas, USA",
+  //   "Villa 8BHK",
+  //   "1500 Sq. Ft.",
+  //   "8BHK Villa in Los Santos, San Andreas, USA",
+  //   "VillaLosSantos#2",
+  //   "VLS",
+  //   10000
+  // );
+
+  // buy shares
+  // await buyShares(
+  //   "0xbA94C268049DD87Ded35F41F6D4C7542b4BdB767",
+  //   "0x851356ae760d987E095750cCeb3bC6014560891C",
+  //   1,
+  //   100
+  // );
+
+  // sell shares
+  //await sellShares(
+  //   0xbA94C268049DD87Ded35F41F6D4C7542b4BdB767,
+  //   0x851356ae760d987E095750cCeb3bC6014560891C,
+  //   10
+  // );
 }
 
 async function deployContracts() {
@@ -121,11 +151,22 @@ async function deployContracts() {
   console.log("\nShares bought transaction hash: ", buyShrares.hash, "\n");
 }
 
-//create a function to list a property take the deploy contract for reference
-async function listProperty(managerAddress,ownerAddress,metadata,propertyName,propertyType,propertyAddress,propertyDescription,propertySize,tokenName,tokenSymbol,propertyShares) {
+async function listProperty(
+  managerAddress,
+  ownerAddress,
+  metadata,
+  propertyName,
+  propertyType,
+  propertyAddress,
+  propertyDescription,
+  propertySize,
+  tokenName,
+  tokenSymbol,
+  propertyShares
+) {
   // Deploy ArxNFT contract
   const ArxNFT = await ethers.getContractFactory("ArxNFT");
-  let ArxToken = await ethers.getContractFactory("ArxToken");
+  //let ArxToken = await ethers.getContractFactory("ArxToken");
   const ArxPropertyManager = await ethers.getContractFactory(
     "ArxPropertyManager"
   );
@@ -133,7 +174,7 @@ async function listProperty(managerAddress,ownerAddress,metadata,propertyName,pr
   const arxPropertyManager = await ArxPropertyManager.attach(managerAddress);
   const ArxNFT_instance = await arxPropertyManager.arxNFTAddress();
   const arxNFT = await ArxNFT.attach(ArxNFT_instance);
-  
+
   const listPropertyTx = await arxPropertyManager.listProperty(
     ownerAddress,
     metadata,
@@ -147,7 +188,7 @@ async function listProperty(managerAddress,ownerAddress,metadata,propertyName,pr
     tokenSymbol,
     { gasLimit: 5000000 }
   );
-  
+
   await listPropertyTx.wait();
   console.log("\nProperty listed successfully.\n");
 
@@ -160,11 +201,92 @@ async function listProperty(managerAddress,ownerAddress,metadata,propertyName,pr
     latestTokenId
   );
   console.log("\nDeployed ArxToken contract address:", arxTokenAddress, "\n");
-
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+async function buyShares(
+  arxTokenAddress,
+  managerAddress,
+  tokenId,
+  shareAmount
+) {
+  let ArxToken = await ethers.getContractFactory("ArxToken");
+  const ArxPropertyManager = await ethers.getContractFactory(
+    "ArxPropertyManager"
+  );
+
+  const signer1 = new ethers.Wallet(process.env.PRIVATE_KEY1, ethers.provider);
+
+  const arxPropertyManager = await ArxPropertyManager.attach(managerAddress);
+  let arxToken = await ArxToken.attach(arxTokenAddress); //.connect(signer1); // add this connect statement if you are using a real network other than localhost
+
+  let SharesStatus = await arxToken.sharesForSale();
+
+  if (!(await SharesStatus)) {
+    console.log("\nSetting the sales for sale for the latest token ID...\n");
+
+    const setSharesForSaleTx = await arxPropertyManager.putSharesForSale(
+      tokenId,
+      ethers.utils.parseEther("10")
+    );
+    await setSharesForSaleTx.wait();
+    console.log(
+      "\nShares set for sale successfully at price of " +
+        ethers.utils.parseEther("10") +
+        " ETH\n"
+    );
+    await buyShares(arxTokenAddress, managerAddress, tokenId, shareAmount);
+  } else {
+    console.log(
+      "\nBuying the shares for the given token ID... from another account\n"
+    );
+
+    let payment =
+      (await arxToken.getTokenPrice(shareAmount)) / 1000000000000000000;
+
+    console.log(
+      "\nPayment: " +
+        (
+          ethers.utils.parseEther(payment.toString()) / 1000000000000000000
+        ).toString() +
+        " ETH\n"
+    );
+
+    let buyShrares = await arxToken.purchaseShare(shareAmount, {
+      value: ethers.utils.parseEther(payment.toString()),
+    });
+    await buyShrares.wait();
+    console.log("\nShares bought transaction hash: ", buyShrares.hash, "\n");
+  }
+}
+
+async function sellShares(arxTokenAddress, managerAddress, shareAmount) {
+  let ArxToken = await ethers.getContractFactory("ArxToken");
+  const ArxPropertyManager = await ethers.getContractFactory(
+    "ArxPropertyManager"
+  );
+
+  const signer1 = new ethers.Wallet(process.env.PRIVATE_KEY1, ethers.provider);
+
+  const arxPropertyManager = await ArxPropertyManager.attach(managerAddress);
+  let arxToken = await ArxToken.attach(arxTokenAddress); //.connect(signer1); // add this connect statement if you are using a real network other than localhost
+  let SharesStatus = await arxToken.canRedeem();
+
+  if (!(await SharesStatus)) {
+    console.log(
+      "The property is not yet sold, Please wait for the sale to happen or contact the custumor support, Thank you"
+    );
+  } else {
+    console.log(
+      "\nSelling the shares for the given token ID... from another account\n"
+    );
+
+    let sellShrares = await arxToken.redeem(shareAmount, { gasLimit: 5000000 });
+    await sellShrares.wait();
+
+    console.log("\nShares sold transaction hash: ", sellShrares.hash, "\n");
+  }
+}
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
